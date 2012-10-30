be7f6c8738f9c3920ec94b2e05c16d0ed1b8b403
Bryce Boe <bbzbryce@gmail.com>
2012-10-30 13:13:37 -0700

    Add Simplified BSD License.

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
+import os
 import re
 from setuptools import setup
 
+MODULE_NAME = 'torrent_checker'
 
-version = re.search("__version__ = '([^']+)'",
-                    open('torrent_checker.py').read()).group(1)
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+VERSION = re.search("__version__ = '([^']+)'",
+                    open('{0}.py'.format(MODULE_NAME)).read()).group(1)
 
-setup(name='torrent checker',
+setup(name=MODULE_NAME,
       author='Bryce Boe',
       author_email='bbzbryce@gmail.com',
+      classifiers=['License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python'],
       description='A program to check for the existance of files for seeding.',
+      entry_points={'console_scripts': ['{0} = {0}:main'.format(MODULE_NAME)]},
       install_requires=['pyrobase', 'pyrocore'],
       keywords=['bittorrent'],
-      py_modules = ['torrent_checker'],
-      version=version,
-      entry_points = {'console_scripts':
-                          ['torrent_checker = torrent_checker:main']}
-
-)
+      license='Simplified BSD License',
+      long_description=README,
+      py_modules=[MODULE_NAME],
+      url='https://github.com/bboe/torrent_checker',
+      version=VERSION)
